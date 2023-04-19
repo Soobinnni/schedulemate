bb5ebc72294ae8c33cabaa7fb46235d0f07f2240
KIM SOOBIN <xpsxm225@naver.com>
2023-04-19 16:48:42 +0900

    first commit

---
+++ src/main/java/com/schedulemate/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.schedulemate.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration 
+public class SecurityConfig {
+
+}
---
+++ src/test/java/com/schedulemate/SchedulemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
