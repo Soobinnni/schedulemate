aa4103f289f6f08ab1fdd633ea8d1b6ee36cc475
KIM SOOBIN <xpsxm225@naver.com>
2023-04-24 15:01:20 +0900

    send setting

--- src/main/java/com/schedulemate/send/controller/SendController.java
+++ src/main/java/com/schedulemate/send/controller/SendController.java
@@ -1,8 +1,45 @@
 package com.schedulemate.send.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Controller
 public class SendController {
+	@Value("${t.apitoken}")
+	private static String tApiToken;
 
+	public static void funcTelegram(String chatId, String text) {
+		String Token = tApiToken;
+
+		BufferedReader in = null;
+
+		try {
+			// 통신
+			URL obj = new URL(
+					"https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chatId + "&text=" + text);
+
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("POST");
+			in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			String line;
+
+			while ((line = in.readLine()) != null) { // response를 차례대로 출력
+				System.out.println("스케줄 전송 성공 : " + line);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null)
+				try {
+					in.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		}
+	}
 }
